f8c01764f09852d0f8985007d62c84694818110e
sayCoffee <986785880@qq.com>
2020-05-10 19:47:05 +0800

    注释部分样式

--- src/App.vue
+++ src/App.vue
@@ -17,14 +17,14 @@
   color: #2c3e50;
   min-height: 100%;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+// #nav {
+//   padding: 30px;
+//   a {
+//     font-weight: bold;
+//     color: #2c3e50;
+//     &.router-link-exact-active {
+//       color: #42b983;
+//     }
+//   }
+// }
 </style>
